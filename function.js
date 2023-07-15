b84d7b5103ffcc94d5b7cfe39f8e8eb4d3fe8256
Mahedee Hasan <mahedee007@gmail.com>
2023-07-16 01:17:47 +0600

    return added

--- function.js
+++ function.js
@@ -10,3 +10,14 @@ function totalMoney(money){
 }
 var taka = 500;
 totalMoney(taka);
+
+// Return 
+//if return is not define then it will be undefined 
+function number(num1, num2){
+    // var num1 = 30;
+    // var num2 = 20;
+    var sum = num1 + num2;
+    return sum;
+}
+var total = number(1, 2);
+console.log('Total', total);
