6edfff05ee7ba78c516081366f19dcc958d101a2
Mahedee Hasan <mahedee007@gmail.com>
2023-07-16 02:56:09 +0600

    more problem

--- function.js
+++ function.js
@@ -51,3 +51,29 @@ function upperCase(value){
 console.log(upperCase('MAHEDEE'));
 
 // ১. একটা ফাংশন লিখবা যেটা ১৩ এর নামতা (multiplication table) আউটপুট হিসেবে দেখাবে। 
+
+// 1) Write a function called foo() which prints “foo” and a function called bar() which prints
+// “bar”. Call function bar() in the foo() function after printing. What will be the output? Now call
+// the foo() to see the output.
+
+// 2) Write a function called make_avg() which will take an three integers and return the
+// average of those values.
+
+// s
+
+// 3) Challenging: Write a function called make_avg() which will take an array of integers and
+// the size of that array and return the average of those values.
+
+// 4) Write a function called odd_even() which takes an integer value and tells whether this
+// value is even or odd. You need to do it in 4 ways:
+// ● Has return + Has parameter
+// ● No return + Has parameter
+
+// Practise Task
+
+// 5) You are in a hurry to go to your school on time. But when you are crossing the road, the
+// traffic signal is red coloured. In this situation, if you try to cross the road, you may be in
+// danger.If you notice a yellow coloured traffic signal, you should stop. If you notice a green
+// coloured traffic signal, you should cross the road. So write a JS code, where there is a
+// variable called signal. Its value can be green, yellow or red & we will get different activities as
+// output depending on the variable. So, hurry up.
