23a302f8002d29a5c853ec8421a430c4751a0f6f
Mahedee Hasan <mahedee007@gmail.com>
2023-07-16 00:19:33 +0600

    function chapter added

---
+++ function.js
@@ -0,0 +1,12 @@
+function startFan(){
+    console.log ('hello world');
+}
+startFan();
+
+// Parameter
+function totalMoney(money){
+    console.log('amar total', money, 'ache');
+
+}
+var taka = 500;
+totalMoney(taka);
